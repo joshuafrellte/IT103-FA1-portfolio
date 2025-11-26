3624f1aed97e1826e5c508a7cfc455543ee3d713
unknown <joshuafrellt@gmail.com>
2025-11-26 23:44:22 +0800

    feat: added animations

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const switchThemeBtn = document.getElementById('switchThemeBtn');
 const profilePic = document.getElementById('profilePic');
-
+const images = document.querySelectorAll('section img');
+const texts = document.querySelectorAll('*');
 
 switchThemeBtn.onclick = function() {
     document.body.classList.toggle('darkMode');
     switchThemeBtn.classList.toggle('darkButton');
     profilePic.classList.toggle('darkProfile');
+    images.forEach(image => {
+        image.classList.toggle('darkImages');
+    })
+    texts.forEach(text => {
+        text.classList.toggle('darkText');
+    })
 }
