098255e91d8800e26b06b9aa0aa97635ceb43d75
unknown <joshuafrellt@gmail.com>
2025-11-26 21:09:14 +0800

    feat: populated certifications section

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const switchThemeBtn = document.getElementById('switchThemeBtn');
+const profilePic = document.getElementById('profilePic');
+
 
 switchThemeBtn.onclick = function() {
     document.body.classList.toggle('darkMode');
+    switchThemeBtn.classList.toggle('darkButton');
+    profilePic.classList.toggle('darkProfile');
 }
